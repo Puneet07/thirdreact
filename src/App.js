51f0ee3fe1de85521fa99f30e13ca342bbbf7750
Puneet07 <puneet777dev@gmail.com>
2017-09-20 18:34:45 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-var test = [
-  {
-    test2: 'test text 1',
-    test1: 'test text 2',
-  }
-]
+
 
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      test
-    };
-  }
 
   render() {
     return (
       <div>
-          {this.state.test.map((test, i) => 
-            <span key={i}>
-              <h1>{test.test1} </h1>
-              <h2>{test.test2}</h2>
-            </span>
-          )}
+       
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+var App = React.createClass({
+    render: function(){
+        return(
+            <div>
+                <div id="header"></div>
+                <div className="container">
+                    <div className="column">
+                        <InboxPane />
+                    </div>
+                    <div className="column">
+                    </div>
+                    <div className="column">
+                    </div>
+                </div>
+            </div>
+        )
+    }
+});
+
+var InboxPane = React.createClass({
+    render: function() {
+        return(
+         <div id="inbox-pane">
+              <h1>Inbox</h1>
+              <table>
+                  <thead>
+                      <tr>
+                          <th>chat Received</th>
+                          <th>Name</th>
+                          <th>Status</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        <InboxItem />
+                    </tbody>
+              </table>
+         </div>
+        );
+    }
+});
+
+var InboxItem = React.createClass({
+    render: function(){
+        return(
+            <tr>
+                <td>5pm</td>
+                <td>Rami loves pizza</td>
+                <td>confirmed</td>
+            </tr>
+        )
+    }
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
