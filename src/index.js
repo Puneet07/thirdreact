3aa8c420bbc593d4bdfc8b5da22d1c86f4e08b6f
puneet07 <puneet777dev@gmail.com>
2017-09-20 22:00:19 +0530

    test
    test

--- src/index.js
+++ src/index.js
@@ -1,33 +1,80 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-var Apps = React.createClass({
-    render: function() {
-        return(
-            <div>
-                <div id="header"></div>
-                <div className="container">
-                    <div className="column">
-                        <InboxPane />
-                    </div>
-                </div>
-            </div>
-        )
-    }
+var samples = require('./sample-data');
+
+var App = React.createClass({
+  getInitialState: function() {
+    return { 
+      "humans": {},
+      "stores": {}
+    };
+  },
+  loadSampleData: function(){
+    this.setState(samples);
+  },
+  render: function() {
+    return (
+      <div>
+        <div id="header"></div>
+        <button onClick={this.loadSampleData}>Load Sample Data</button>
+        <div className="container">
+          <div className="column">
+            <InboxPane humans={this.state.humans}/>
+          </div>
+          <div className="column"></div>
+          <div className="column"></div>
+        </div>
+      </div>
+    )
+  }
 });
 
 var InboxPane = React.createClass({
-    render: function() {
-        return(
-            <div id="inbox-pane">
-                <h1>Inbox</h1>
-            </div>
-        )
-    }
-})
+  renderConvoSum: function(human){
+    return <InboxItem key={human} index={human} details={this.props.humans[human]} />;
+  },
+  render : function() {
+    return (
+      <div id="inbox-pane">
+        <h1>Inbox</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Chat Received</th>
+              <th>Name</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Object.keys(this.props.humans).map(this.renderConvoSum)}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+});
 
-ReactDOM.render(<Apps />, document.getElementById('root'));
+var InboxItem = React.createClass({ 
+  sortByDate: function(a, b) {
+      return a.time>b.time ? -1 : a.time<b.time ? 1 : 0;
+  },
+  messageSummary: function(conversations){
+    var lastMessage = conversations.sort(this.sortByDate)[0];
+    return lastMessage.who + ' said: "' + lastMessage.text + '" @ ' + lastMessage.time.toDateString();
+  },
+  render: function(){
+    return (
+      <tr>
+        <td>{this.messageSummary(this.props.details.conversations)}</td>
+        <td>{this.props.index}</td>
+        <td>{this.props.details.orders.sort(this.sortByDate)[0].status}</td>
+      </tr>
+    )
+  }
+});
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
